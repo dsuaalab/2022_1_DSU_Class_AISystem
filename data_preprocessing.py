a44be78718106f4f5966d6b3c6b3206861123c75
SM <choiseokmin@naver.com>
2022-06-17 03:46:34 +0900

    Update data and data_preprocessing.py
    데이터 연령대 삭제 및 데이터 업종 코드 문자열 삭제

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,12 +5,17 @@ df = pd.read_csv('dataset/data.csv', encoding='euc-kr')
 
 df.rename(columns={'내국인업종코드(SB_UPJONG_CD)': 'SB_UPJONG_CD'}, inplace=True)
 df.rename(columns={'성별(SEX_CCD)': 'SEX_CCD'}, inplace=True)
-df.rename(columns={'연령대별(AGE_GB)': 'AGE_GB'}, inplace=True)
+#df.rename(columns={'연령대별(AGE_GB)': 'AGE_GB'}, inplace=True)
 df.rename(columns={'카드이용건수(USECT_CORR)': 'USECT_CORR'}, inplace=True)
+
+df['SB_UPJONG_CD'] = df['SB_UPJONG_CD'].str.replace(r'\D', '')
+
 
 data = df.dropna(how='any')
 
 data = data.reset_index(drop=True)
+
+
 
 data.to_csv('dataset/processed_data.csv', encoding='euc-kr', index = False)
 
